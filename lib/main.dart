01d515d625fd8db5b177b67dd6df751f49eeec72
canisterism <ohtthstc@gmail.com>
2020-03-18 16:34:54 +0900

    textのborderの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'scoped_models/serch_list.dart';
 import 'package:scoped_model/scoped_model.dart';
+
+import 'package:html/dom.dart' as dom;
 
 void main() => runApp(MyApp());
 
@@ -31,7 +34,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _children = [MembersScreen(), HtmlScreen()];
+
+  int _currentIndex = 0;
+
+  _onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTapped,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.people), title: Text('members')),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.code),
+              title: Text('html'),
+            )
+          ]),
+    );
+  }
+}
+
+class HtmlScreen extends StatelessWidget {
+  const HtmlScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('HTML検証')),
+      body: Container(
+        child: SingleChildScrollView(
+            child: Column(children: [
+          Html(data: '<h1>タイトルタイトル</h1>'),
+          Html(data: '<h2>タイトルタイトル</h2>'),
+          Html(data: '<h3>タイトルタイトル</h3>'),
+          Html(data: '<h4>タイトルタイトル</h4>'),
+          Html(data: '<p>タイトルタイトル</p>'),
+          Html(
+            data: '<p class="border">タイトルタイトル</p>',
+            useRichText: false,
+            customRender: (node, children) {
+              if (node is dom.Element && node.className == "border") {
+                return Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                    child: Column(children: children));
+              } else {
+                return null;
+              }
+            },
+          ),
+        ])),
+      ),
+    );
+  }
+}
+
+class MembersScreen extends StatelessWidget {
+  const MembersScreen({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
